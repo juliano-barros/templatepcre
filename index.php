a609c125da6a1c6adc8cfdf756fc07d947e43ba4
Juliano <julianorafael@gmail.com>
2018-03-24 16:24:14 -0300

    Deleting echo debugs

--- index.php
+++ index.php
@@ -12,27 +12,22 @@ $stuff = [ [ "Thing" => "roses", "Desc"  => "red" ],
 
 $file = $_FILES["file"]["tmp_name"];
 
-echo "aqui 1";
 /**
  * @string
  */
 $fileName = rand(1,50000) . date("YmdHis");
 
-echo "aqui 2";
-move_uploaded_file( $file, 'temp/' . $fileName . '.tmpl');
-echo "aqui 3";
+move_uploaded_file( $file, $fileName . '.tmpl');
 
 $template = new Template(['Name' => $name, 'Stuff'=> $stuff]);
-echo "aqui 4";
 // You can add variable like this
 //$template->addVariable('Name', $Name);
 //$template->addVariable('Stuff', $Stuff);
 // If your views are in a different path you can set your base path views
 //$template->setPath( 'src/views/');
-$template->render('temp/'.$fileName);
-echo "aqui 5";
+$template->render($fileName);
 
-unlink( 'temp/' . $fileName . '.tmpl');
+unlink( $fileName . '.tmpl');
 
 ?>
 
