4dae6792aab15aba7e4d4271c376e6f0b5cdf6df
Juliano <julianorafael@gmail.com>
2018-03-24 16:01:58 -0300

    Test what is going on

--- index.php
+++ index.php
@@ -12,23 +12,27 @@ $stuff = [ [ "Thing" => "roses", "Desc"  => "red" ],
 
 $file = $_FILES["file"]["tmp_name"];
 
-
+echo "aqui 1";
 /**
  * @string
  */
 $fileName = rand(1,50000) . date("YmdHis");
 
+echo "aqui 2";
 move_uploaded_file( $file, 'temp/' . $fileName . '.tmpl');
+echo "aqui 3";
 
 $template = new Template(['Name' => $name, 'Stuff'=> $stuff]);
+echo "aqui 4";
 // You can add variable like this
 //$template->addVariable('Name', $Name);
 //$template->addVariable('Stuff', $Stuff);
 // If your views are in a different path you can set your base path views
 //$template->setPath( 'src/views/');
 $template->render('temp/'.$fileName);
+echo "aqui 5";
 
-unlink( 'temp/' . $fileName . '.tmpl');
+//unlink( 'temp/' . $fileName . '.tmpl');
 
 ?>
 
